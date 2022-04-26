81fee55db1ccaf7d3bbfc50ef85524b053ebb8c0
Alexia Auddino <alexia.auddino@student.isae-supaero.fr>
2022-04-26 12:29:36 +0200

    Creation de la carte dans views et ajout de la carte dans le template de la page de visualisation des différentes sorties pour un itinéraire

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
 from .forms import SortieForm
-from .models import Itineraire, Sortie
+from .models import Itineraire, Sortie, Map
+import folium
 
 # Create your views here.
 
@@ -54,9 +55,18 @@ def sorties(request, itineraire_id):
     try:
         itineraire= Itineraire.objects.get(pk=itineraire_id)
         sorties_list = Sortie.objects.filter(itineraire=itineraire).all()
+        try :
+            map_infos = Map.objects.get(itineraire=itineraire)
+            map = folium.Map([map_infos.zone_geo_latitude, map_infos.zone_geo_longitude], width='100%', height='70%',position='relative', min_zoom=0, max_zoom=50, zoom_start=14)
+            folium.Marker(location=[map_infos.latitude_depart, map_infos.longitude_depart],tooltip='Départ : ' + map_infos.depart).add_to(map)
+            folium.Marker(location=[map_infos.latitude_arrivee, map_infos.longitude_arrivee],tooltip='Arrivée : ' + map_infos.arrivee).add_to(map)
+            map=map._repr_html_() #updated
+            context = {'itineraire': itineraire, 'sorties_list': sorties_list, 'map': map}
+        except Map.DoesNotExist : 
+            context = {'itineraire': itineraire, 'sorties_list': sorties_list}
     except Itineraire.DoesNotExist:
         raise Http404("L'itinéraire n'existe pas")
-    return render(request, 'itineraires/sorties.html', {'itineraire': itineraire, 'sorties_list': sorties_list})
+    return render(request, 'itineraires/sorties.html', context)
 
 # Vue accessible pour les utilisateurs connectés ou non avec des options d'ajout et de modification 
 # seulement pour les utilisateurs connectés
