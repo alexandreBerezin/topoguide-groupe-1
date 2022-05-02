eeee4b79078a294b102c95e05a11041258e3d854
Alexia Auddino <alexia.auddino@student.isae-supaero.fr>
2022-05-02 09:36:18 +0200

    Correction- redirection sur le détail d'une sortie lors de la création d'une nouvelle sortie

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,7 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 
 from .forms import SortieForm
 from .models import Itineraire, Sortie, Map
@@ -113,17 +112,20 @@ def nouvelle_sortie(request, itineraire_id):
     submitted = False
     if request.method == 'POST':
         form = SortieForm(request.POST)
-        if form.is_valid :
+        if form.is_valid() :
             form = form.save(commit=False)
             form.utilisateur = request.user
             form.itineraire = Itineraire.objects.get(pk=itineraire_id)
             form.save()
             submitted = True
+            sortie_id = form.id
+            context = {'new':new,'form' : form, 'itineraire_id' : itineraire_id, 'sortie_id' : sortie_id, 'submitted' : submitted}
     else : 
         form = SortieForm
         if submitted in request.GET:
             submitted = True
-    return render(request, 'itineraires/nouvelle_sortie.html', {'new': new,'form' : form, 'itineraire_id' : itineraire_id, 'submitted' : submitted})
+        context = {'new':new,'form' : form, 'itineraire_id' : itineraire_id,'submitted' : submitted}
+    return render(request, 'itineraires/nouvelle_sortie.html', context)
 
 # vue accessible seulement pour un utilisateur qui possède un compte et est authentifié 
 @login_required
@@ -153,4 +155,5 @@ def modif_sortie(request, itineraire_id, sortie_id):
             submitted=True
     else : 
         form = SortieForm(request.POST or None, instance=sortie)
-    return render(request, 'itineraires/nouvelle_sortie.html', {'new':new,'form' : form, 'itineraire_id' : itineraire_id, 'sortie_id' : sortie_id, 'submitted' : submitted})
+    context = {'new':new,'form' : form, 'itineraire_id' : itineraire_id, 'sortie_id' : sortie_id, 'submitted' : submitted}
+    return render(request, 'itineraires/nouvelle_sortie.html', context)
